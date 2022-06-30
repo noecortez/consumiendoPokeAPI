f8ebd93f74a6acec106fd86671253b5371a3a3ce
Noe Cortez <corteznoe3@gmail.com>
2022-06-29 21:26:31 -0600

    Buscador, spinner y alerta creados

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const containerCards = document.getElementById('container-cards');
 const btnPrevius = document.getElementById('previus');
 const btnNext = document.getElementById('next');
 
+const formSearch = document.querySelector('form');
+const searchBox = document.getElementById('search-box');
+
 let limit = 5;
 let offset = 1;
 
@@ -23,6 +26,27 @@ btnNext.addEventListener('click', () => {
   btnPrevius.className = 'page-item';
 });
 
+formSearch.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  if (searchBox.value == '')
+    throw createAlert('Favor, complete el campo para hacer la busqueda', 'danger');
+
+  removeChildNodes(containerCards);
+  document.querySelector('.spinner-border').classList.remove('hidden');
+
+  const pokemon = await getFetchPokemonData(searchBox.value.toLowerCase(), 'pokemon');
+
+  if (pokemon) {
+    document.querySelector('.spinner-border').classList.add('hidden');
+    createCard(pokemon);
+  } else {
+    document.querySelector('.spinner-border').classList.add('hidden');
+    fetchPokemons(offset, limit);
+  }
+
+  searchBox.value = '';
+});
 
 async function fetchPokemons(offset, limit) {
   for (let index = offset; index <= offset + limit; index++) {
@@ -33,6 +57,12 @@ async function fetchPokemons(offset, limit) {
 
 async function getFetchPokemonData(id, endpoint) {
   const response = await fetch(`${urlPokemons}/${endpoint}/${id}`);
+
+  if (response.status == 404) {
+    createAlert('El recurso no fue encontrado', 'danger');
+    return;
+  }
+
   const pokemonInfo = await response.json();
   return pokemonInfo;
 }
@@ -108,6 +138,7 @@ function getIdURL(url) {
 
 async function createModal(id) {
   const containerModal = document.querySelector('#staticBackdrop .modal-dialog.modal-dialog-centered');
+  containerModal.innerHTML = '';
 
   const pokemonInfo = await getFetchPokemonData(id, 'pokemon');
   const {
@@ -190,6 +221,26 @@ function removeChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
+}
+
+function createAlert(message, color) {
+  const templateAlert = `
+  <div class="alert alert-${color} alert-dismissible fade show d-flex align-items-center" role="alert">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+      <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+    </svg>
+    <div>
+      ${message}
+    </div>
+
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+  `;
+
+  document.getElementById('alertContainer').innerHTML = templateAlert;
+  setTimeout(() => {
+    document.getElementById('alertContainer').innerHTML = '';
+  }, 4000);
 }
 
 
